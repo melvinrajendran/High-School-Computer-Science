af6a140880cba5205b21e0e2274138641fbf4176
Melvin Rajendran <44681827+melvinrajendran@users.noreply.github.com>
2020-09-10 22:01:00 -0400

    Add files via upload

---
+++ VR and Game Development:Demo1/Practice.java
@@ -0,0 +1,7 @@
+public class Practice{
+    public static void main (String[] args){
+        System.out.println("*\n**\n***\n****\n*****");
+        System.out.println("My\tname\tis\tdhruv");
+        System.out.println("H\\E\\L\\L\\O");
+    }
+}
