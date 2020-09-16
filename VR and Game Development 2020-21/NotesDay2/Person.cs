3efa1abe277af330ef785e2ed389ebdfad5cc138
Melvin Rajendran <44681827+melvinrajendran@users.noreply.github.com>
2020-09-15 20:20:07 -0400

    Add files via upload

---
+++ VR and Game Development 2020-21/NotesDay2/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace NotesDay2
+{
+    class Person
+    {
+        private string firstName;
+        private string eyeColor;
+        private int daysAlive;
+
+        public string LastName { get; set; }
+
+        public float YearsAlive
+        {
+            get
+            {
+                return (float)(daysAlive / 365.0);
+            }
+
+            set
+            {
+                daysAlive = (int)(value * 365);
+            }
+        }
+
+        public Person(string firstName, int daysAlive)
+        {
+            this.firstName = firstName;
+            this.daysAlive = daysAlive;
+        }
+
+        // Constructor Chaining
+        public Person(string firstName, string eyeColor, int daysAlive)
+            :this(firstName, daysAlive)
+        {
+            this.eyeColor = eyeColor;
+        }
+
+        public override string ToString()
+        {
+            return firstName + " " + LastName + " has " + eyeColor + " eyes and has been alive for " + YearsAlive + " years.";
+        }
+    }
+
+}
---
+++ VR and Game Development 2020-21/NotesDay2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace NotesDay2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person("Melvin", "Brown", 10000);
+            p. = "Rajendran";
+
+            Console.WriteLine(p.LastName);
+            Console.WriteLine(p.YearsAlive);
+            p.YearsAlive = 17;
+            Console.WriteLine(p);
+        }
+    }
+}
