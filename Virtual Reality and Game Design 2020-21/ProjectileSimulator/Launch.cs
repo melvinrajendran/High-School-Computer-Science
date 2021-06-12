755ffbdd5fd300f9461f256f9ac90c29c1e20e78
Melvin Rajendran <44681827+melvinrajendran@users.noreply.github.com>
2021-06-12 12:20:50 -0400

    Add files via upload

---
+++ Virtual Reality and Game Design 2020-21/ProjectileSimulator/Launch.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launch : MonoBehaviour
+{
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * 25, ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Virtual Reality and Game Design 2020-21/ProjectileSimulator/Spawn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public GameObject gameObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            gameObject.SetActive(true);
+            Instantiate(gameObject, transform.position, transform.rotation);
+        }
+    }
+}
